94c5e6a1c502827e1edf1eaf5b8adcd732a5187f
1R1an1 <iangamer7764@gmail.com>
2026-01-08 02:58:59 -0300

    Añadiendo opcion para que la app se abra con windows
    Se añadio un estilo para los checkbox
    Se agrego un checkbox para habilitar o deshabilitar el inicio con Windows
    El estado del checkbox se sincroniza con el Registro
    Si la app ya se encuentra en segundo plano y se abre nuevamente, se restaura la ventana en lugar de iniciar otra instancia

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using FortiCrypts;
 using Games_Launcher.Core;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -12,9 +14,27 @@ namespace Games_Launcher
     {
         public static MainWindow window;
         private bool EnableAutoSave = false;
-        protected override void OnStartup(StartupEventArgs e)
+
+		private static Mutex _mutex;
+		private static EventWaitHandle _showEvent;
+
+		private const string MutexName = "GamesLauncher_SingleInstance";
+		private const string ShowEventName = "GamesLauncher_ShowWindow";
+
+		protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
+			bool createdNew;
+			_mutex = new Mutex(true, MutexName, out createdNew);
+
+			if (!createdNew)
+			{
+				NotifyInstance();
+				Shutdown();
+				return;
+			}
+
+
+			base.OnStartup(e);
             CryptoUtils.iterations = 2500;
             GamesInfo.CheckFileNames();
             GamesInfo.LoadGamesData();
@@ -28,19 +48,64 @@ namespace Games_Launcher
                         GamesInfo.SaveGamesData();
                 }
             });
+
             window = new MainWindow();
+            if (e.Args.Contains("-background"))
+                Ocultar();
+            else
+                window.Show();
+
             MainWindow = window;
-            MainWindow.Show();
             App.Current.Exit += Current_Exit;
 
             GameMonitor.StartLoop();
             EnableAutoSave = true;
 
+            CreateListener();
+
             //reference windoww = new reference();
             ////MainWindow = windoww;
             //windoww.Show();
         }
-        private void Current_Exit(object sender, ExitEventArgs e)
+
+        private void CreateListener()
+        {
+            _showEvent = new EventWaitHandle(false, EventResetMode.AutoReset, ShowEventName);
+
+			Task.Run(() =>
+			{
+				while (true)
+				{
+					_showEvent.WaitOne();
+                    Mostrar();
+				}
+			});
+		}
+        private void NotifyInstance()
+        {
+			EventWaitHandle.OpenExisting(ShowEventName).Set();
+		}
+
+        private void Mostrar()
+        {
+			Dispatcher.Invoke(() =>
+			{
+				window.ShowInTaskbar = true;
+				window.Visibility = Visibility.Visible;
+				window.WindowState = WindowState.Normal;
+				window.Activate();
+			});
+		}
+
+        private void Ocultar()
+        {
+			window.ShowInTaskbar = false;
+			window.WindowState = WindowState.Minimized;
+			window.Visibility = Visibility.Hidden;
+            window.Hide();
+		}
+
+		private void Current_Exit(object sender, ExitEventArgs e)
         {
             window.InvokeEvent();
             GamesInfo.SaveGamesData();
--- Views/ConfigView.xaml.cs
+++ Views/ConfigView.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,55 @@ namespace Games_Launcher.Views
     /// </summary>
     public partial class ConfigView : UserControl
     {
-        public ConfigView()
+		private const string RunKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
+		private const string ValueName = "Games Launcher";
+
+		private bool _isLoading;
+
+		public ConfigView()
         {
             InitializeComponent();
-        }
-    }
+			_isLoading = true;
+			CBStartWindows.IsChecked = IsAutoStartEnabled();
+			_isLoading = false;
+		}
+
+		public static bool IsAutoStartEnabled()
+		{
+			using (var key = Registry.CurrentUser.OpenSubKey(RunKey, false))
+			{
+				if (key == null)
+					return false;
+
+				var value = key.GetValue(ValueName);
+				if (value == null)
+					return false;
+
+				string exePath = Assembly.GetExecutingAssembly().Location;
+				return value.ToString().Contains(exePath);
+			}
+		}
+
+        private void CBStartWindows_Checked(object sender, RoutedEventArgs e)
+        {
+			if (_isLoading) return;
+			using (var key = Registry.CurrentUser.OpenSubKey(RunKey, true))
+			{
+				key.SetValue(
+					ValueName,
+					$"\"{Assembly.GetExecutingAssembly().Location}\" -background"
+				);
+			}
+		}
+
+        private void CBStartWindows_Unchecked(object sender, RoutedEventArgs e)
+        {
+			if (_isLoading) return;
+			using (var key = Registry.CurrentUser.OpenSubKey(RunKey, true))
+			{
+				if (key.GetValue(ValueName) != null)
+					key.DeleteValue(ValueName);
+			}
+		}
+	}
 }
