2e4fb6652401597cf3a73358eff1f1c27e6b759c
1R1an1 <iangamer7764@gmail.com>
2026-01-08 04:30:25 -0300

    Se añadio un boton para añadir la app a menu inicio

--- Views/ConfigView.xaml.cs
+++ Views/ConfigView.xaml.cs
@@ -1,23 +1,27 @@
-﻿using Microsoft.Win32;
+﻿using IWshRuntimeLibrary;
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace Games_Launcher.Views
 {
-    /// <summary>
-    /// Lógica de interacción para ConfigView.xaml
-    /// </summary>
-    public partial class ConfigView : UserControl
-    {
+	/// <summary>
+	/// Lógica de interacción para ConfigView.xaml
+	/// </summary>
+	public partial class ConfigView : UserControl
+	{
 		private const string RunKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
 		private const string ValueName = "Games Launcher";
 
 		private bool _isLoading;
 
 		public ConfigView()
-        {
-            InitializeComponent();
+		{
+			InitializeComponent();
 			_isLoading = true;
 			CBStartWindows.IsChecked = IsAutoStartEnabled();
 			_isLoading = false;
@@ -39,8 +43,8 @@ namespace Games_Launcher.Views
 			}
 		}
 
-        private void CBStartWindows_Checked(object sender, RoutedEventArgs e)
-        {
+		private void CBStartWindows_Checked(object sender, RoutedEventArgs e)
+		{
 			if (_isLoading) return;
 			using (var key = Registry.CurrentUser.OpenSubKey(RunKey, true))
 			{
@@ -51,14 +55,36 @@ namespace Games_Launcher.Views
 			}
 		}
 
-        private void CBStartWindows_Unchecked(object sender, RoutedEventArgs e)
-        {
+		private void CBStartWindows_Unchecked(object sender, RoutedEventArgs e)
+		{
 			if (_isLoading) return;
 			using (var key = Registry.CurrentUser.OpenSubKey(RunKey, true))
 			{
 				if (key.GetValue(ValueName) != null)
 					key.DeleteValue(ValueName);
 			}
+		}
+
+		private void BTNStartMenu_Click(object sender, RoutedEventArgs e)
+		{
+			string startmenu = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Programs", "1R1an1");
+			string startmenulnk = Path.Combine(startmenu, "Games Launcher.lnk");
+			var shell = new WshShell();
+
+			if (!Directory.Exists(startmenu)) Directory.CreateDirectory(startmenu);
+			IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(startmenulnk);
+
+			string exePath = Process.GetCurrentProcess().MainModule.FileName;
+
+			Console.WriteLine(startmenulnk);
+			Console.WriteLine(Directory.GetCurrentDirectory());
+
+			shortcut.TargetPath = exePath;
+			shortcut.WorkingDirectory = Path.GetDirectoryName(exePath);
+			shortcut.Description = "Launcher de juegos";
+			shortcut.IconLocation = exePath;
+
+			shortcut.Save();
 		}
 	}
 }
