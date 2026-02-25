0c860a82f3eb296edf4967d119f746602119235a
1R1an1 <XZ1R14N17764@proton.me>
2026-02-25 03:15:32 -0300

    Separando logica de ConfigView

---
+++ Core/ConfigApp.cs
@@ -0,0 +1,55 @@
+﻿using IWshRuntimeLibrary;
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Games_Launcher.Core
+{
+	public class ConfigApp
+	{
+		private const string RunKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
+		private const string ValueName = "Games Launcher";
+
+		public static bool IsAutoStartEnabled()
+		{
+			using var key = Registry.CurrentUser.OpenSubKey(RunKey, false);
+			if (key == null) return false;
+			var value = key.GetValue(ValueName);
+			if (value == null) return false;
+			string exePath = Assembly.GetExecutingAssembly().Location;
+			return value.ToString().Contains(exePath);
+		}
+
+		public static void EnableAutoStart()
+		{
+			using var key = Registry.CurrentUser.OpenSubKey(RunKey, true);
+			key.SetValue(ValueName, $"\"{Assembly.GetExecutingAssembly().Location}\" -background");
+		}
+
+		public static void DisableAutoStart()
+		{
+			using var key = Registry.CurrentUser.OpenSubKey(RunKey, true);
+			if (key.GetValue(ValueName) != null)
+				key.DeleteValue(ValueName);
+		}
+
+		public static void CreateStartMenuShortcut()
+		{
+			string startmenu = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Programs", "1R1an1");
+			string startmenulnk = Path.Combine(startmenu, "Games Launcher.lnk");
+			var shell = new WshShell();
+			if (!Directory.Exists(startmenu)) Directory.CreateDirectory(startmenu);
+
+			IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(startmenulnk);
+			string exePath = Process.GetCurrentProcess().MainModule.FileName;
+
+			shortcut.TargetPath = exePath;
+			shortcut.WorkingDirectory = Path.GetDirectoryName(exePath);
+			shortcut.Description = "Launcher de juegos";
+			shortcut.IconLocation = exePath;
+			shortcut.Save();
+		}
+	}
+}
--- Views/ConfigView.xaml.cs
+++ Views/ConfigView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using IWshRuntimeLibrary;
+﻿using Games_Launcher.Core;
+using IWshRuntimeLibrary;
 using Microsoft.Win32;
 using System;
 using System.Diagnostics;
@@ -23,68 +24,22 @@ namespace Games_Launcher.Views
 		{
 			InitializeComponent();
 			_isLoading = true;
-			CBStartWindows.IsChecked = IsAutoStartEnabled();
+			CBStartWindows.IsChecked = ConfigApp.IsAutoStartEnabled();
 			_isLoading = false;
-		}
-
-		public static bool IsAutoStartEnabled()
-		{
-			using (var key = Registry.CurrentUser.OpenSubKey(RunKey, false))
-			{
-				if (key == null)
-					return false;
-
-				var value = key.GetValue(ValueName);
-				if (value == null)
-					return false;
-
-				string exePath = Assembly.GetExecutingAssembly().Location;
-				return value.ToString().Contains(exePath);
-			}
 		}
 
 		private void CBStartWindows_Checked(object sender, RoutedEventArgs e)
 		{
 			if (_isLoading) return;
-			using (var key = Registry.CurrentUser.OpenSubKey(RunKey, true))
-			{
-				key.SetValue(
-					ValueName,
-					$"\"{Assembly.GetExecutingAssembly().Location}\" -background"
-				);
-			}
+			ConfigApp.EnableAutoStart();
 		}
 
 		private void CBStartWindows_Unchecked(object sender, RoutedEventArgs e)
 		{
 			if (_isLoading) return;
-			using (var key = Registry.CurrentUser.OpenSubKey(RunKey, true))
-			{
-				if (key.GetValue(ValueName) != null)
-					key.DeleteValue(ValueName);
-			}
+			ConfigApp.DisableAutoStart();
 		}
 
-		private void BTNStartMenu_Click(object sender, RoutedEventArgs e)
-		{
-			string startmenu = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Programs", "1R1an1");
-			string startmenulnk = Path.Combine(startmenu, "Games Launcher.lnk");
-			var shell = new WshShell();
-
-			if (!Directory.Exists(startmenu)) Directory.CreateDirectory(startmenu);
-			IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(startmenulnk);
-
-			string exePath = Process.GetCurrentProcess().MainModule.FileName;
-
-			Console.WriteLine(startmenulnk);
-			Console.WriteLine(Directory.GetCurrentDirectory());
-
-			shortcut.TargetPath = exePath;
-			shortcut.WorkingDirectory = Path.GetDirectoryName(exePath);
-			shortcut.Description = "Launcher de juegos";
-			shortcut.IconLocation = exePath;
-
-			shortcut.Save();
-		}
+		private void BTNStartMenu_Click(object sender, RoutedEventArgs e) => ConfigApp.CreateStartMenuShortcut();
 	}
 }
