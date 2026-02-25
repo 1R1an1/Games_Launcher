eca5ec93ceb7cbb7e5cc7ed41cfd4bf664040b4b
1R1an1 <XZ1R14N17764@proton.me>
2026-02-25 05:01:05 -0300

    Se añade ObservableObject que implementa INotifyPropertyChanged

--- Model/GameViewModel.cs
+++ Model/GameViewModel.cs
@@ -1,14 +1,11 @@
 ﻿using Games_Launcher.Core;
 using System;
-using System.ComponentModel;
 using System.Windows.Media;
 
 namespace Games_Launcher.Model
 {
-    public class GameViewModel : INotifyPropertyChanged
+    public class GameViewModel : ObservableObject
 	{
-		public event PropertyChangedEventHandler PropertyChanged;
-		private void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
 		public readonly GameModel _game;
 
@@ -18,13 +15,13 @@ namespace Games_Launcher.Model
 		public string Name
 		{
 			get => _game.Name;
-			set { _game.Name = value; OnPropertyChanged(nameof(Name)); }
+			set => SetProperty(ref _game.Name, value);
 		}
 
 		public string Path
 		{
 			get => _game.Path;
-			set { _game.Path = value; OnPropertyChanged(nameof(Path)); OnPropertyChanged(nameof(Icon)); }
+			set { SetProperty(ref _game.Path, value); OnPropertyChanged(nameof(Icon)); }
 		}
 
 		public ImageSource Icon => GameFunctions.GetGameIcon(Path);
@@ -32,19 +29,19 @@ namespace Games_Launcher.Model
 		public string Parameters
 		{
 			get => _game.Parameters;
-			set { _game.Parameters = value; OnPropertyChanged(nameof(Parameters)); }
+			set => SetProperty(ref _game.Parameters, value);
 		}
 
 		public DateTime LastPlayed
 		{
 			get => _game.LastPlayed;
-			set { _game.LastPlayed = value; OnPropertyChanged(nameof(LastPlayed)); OnPropertyChanged(nameof(LastPlayedFormatted)); }
+			set { SetProperty(ref _game.LastPlayed, value); OnPropertyChanged(nameof(LastPlayedFormatted)); }
 		}
 
 		public TimeSpan PlayTime
 		{
 			get => _game.PlayTime;
-			set { _game.PlayTime = value; OnPropertyChanged(nameof(PlayTime)); OnPropertyChanged(nameof(PlayTimeFormatted)); }
+			set { SetProperty(ref _game.PlayTime, value); OnPropertyChanged(nameof(PlayTimeFormatted)); }
 		}
 
 		public string LastPlayedFormatted => GameFunctions.UltimaVezJugado(LastPlayed);
---
+++ Model/ObservableObject.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Games_Launcher.Model
+{
+	public abstract class ObservableObject : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		/// <summary>
+		/// Método helper para asignar un valor a un campo privado y disparar el evento PropertyChanged automáticamente.
+		/// Esto evita repetir siempre la misma estructura de setter con OnPropertyChanged.
+		/// Uso típico:
+		/// private bool _isRunning;
+		/// public bool IsRunning
+		/// {
+		///     get => _isRunning;
+		///     set => SetProperty(ref _isRunning, value);
+		/// }
+		/// </summary>
+		/// <typeparam name="T">Tipo de la propiedad</typeparam>
+		/// <param name="field">Referencia al campo privado que guarda el valor</param>
+		/// <param name="value">Nuevo valor a asignar</param>
+		/// <param name="propertyName">Nombre de la propiedad (opcional, lo llena CallerMemberName automáticamente)</param>
+		/// <returns>Devuelve true si el valor cambió, false si era igual</returns>
+		protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+		{
+			if (Equals(field, value))
+				return false;
+
+			field = value;
+			OnPropertyChanged(propertyName);
+			return true;
+		}
+	}
+}
